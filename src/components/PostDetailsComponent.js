f15c07012fc317fc97ac1733efd2719ecde3cd6d
RoseveIT <62Bip23_Sorta557_qq>
2022-07-10 19:13:06 +0300

    ls5

--- src/components/PostDetailsComponent.js
+++
@@ -1,7 +0,0 @@
-export default function PostDetailsComponent() {
-    return (
-        <div>
-            PostDetailsComponents
-        </div>
-    );
-}
--- src/components/UserComponent.js
+++
@@ -1,9 +0,0 @@
-import {Link} from "react-router-dom";
-
-export default function UserComponent({user}) {
-    return (
-        <div>
-            {user.name} - <span><Link to={user.id.toString()}>user post</Link></span>
-        </div>
-    );
-}
